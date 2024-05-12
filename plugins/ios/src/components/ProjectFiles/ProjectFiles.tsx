e7b378a3ce4c1925bdad5f2428d393fa1ef07125
Andrei Lasenka <laughugin@icloud.com>
2024-05-12 17:36:18 +0200

    added project file section to project overview

---
+++ plugins/ios/src/components/ProjectFiles/ProjectFiles.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+export const ProjectFiles = () => {
+    return (
+        <Grid>
+            <h1> Project Files </h1>
+            <p> Soon to be implmented ... </p>
+        </Grid>
+    )
+}
--- plugins/ios/src/components/ProjectOverview/ProjectOverview.tsx
+++ plugins/ios/src/components/ProjectOverview/ProjectOverview.tsx
@@ -3,8 +3,7 @@ import { Button, Dialog, DialogTitle, DialogContent, DialogActions, Grid, IconBu
 import { makeStyles } from '@material-ui/core/styles';
 import { CommentSection } from '../CommentSection';
 import { ProjectInfo } from '../ProjectInfo';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+import { ProjectFiles } from '../ProjectFiles';
 
 type Props = {
   open: boolean;
@@ -40,6 +39,7 @@ export const ProjectOverview = ({ open, handleCloseDialog, project }: Props) => 
       <DialogTitle>Project Overview</DialogTitle>
       <DialogContent>
         <ProjectInfo project={project}  /> {/* Pass isEditable state to ProjectInfo */}
+        <ProjectFiles/>
         <CommentSection />
       </DialogContent>
     </Dialog>
